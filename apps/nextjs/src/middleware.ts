4168451685205ef7d158cf36f1d57a11a4ebfb60
juancarlosf2 <juan_carlosf2@hotmail.com>
2023-08-22 21:01:07 -0400

    ignore trpc from the private routes

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -2,6 +2,12 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  ignoredRoutes: [
+    "/((?!api|trpc))(_next|.+..+)(.*)",
+    "/api/trpc/post.all",
+    "/api/trpc/post.byId",
+    "/api/trpc/post.create",
+  ],
 });
 
 export const config = {
